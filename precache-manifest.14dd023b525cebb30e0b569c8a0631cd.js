56f4a9d732051067bcdabbdfc13039cfd7b06302
VicenteMelin <vtmelin@uc.cl>
2020-05-16 22:15:26 -0400

    Updates

---
+++ precache-manifest.14dd023b525cebb30e0b569c8a0631cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ccf0ed6307446bcd87632eaf0df8a1",
+    "url": "/Tarea3_TallerInt/index.html"
+  },
+  {
+    "revision": "10b9c0fb33e29bc3b069",
+    "url": "/Tarea3_TallerInt/static/css/main.fec874d2.chunk.css"
+  },
+  {
+    "revision": "41ccc0ad69c5e5927789",
+    "url": "/Tarea3_TallerInt/static/js/2.da787f41.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/Tarea3_TallerInt/static/js/2.da787f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b9c0fb33e29bc3b069",
+    "url": "/Tarea3_TallerInt/static/js/main.13b123de.chunk.js"
+  },
+  {
+    "revision": "0c21f0c5d8643a50cd3d",
+    "url": "/Tarea3_TallerInt/static/js/runtime-main.e8706b59.js"
+  }
+]);
